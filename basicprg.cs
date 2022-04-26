1fc872123464f70bf9c342e4926852577dbcf6f5
Muskandhamani1108 <99160747+Muskandhamani1108@users.noreply.github.com>
2022-04-26 19:40:24 +0530

    Add files via upload

---
+++ basicprg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace basic1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO!!!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ breakinwhile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace while1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 10)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                    if (i == 6)
+                    {
+                        break;
+                    }
+                }
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
