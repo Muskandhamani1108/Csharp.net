950ffaa7dbf5a857b535f42e1e97ec77b2e425db
Muskandhamani1108 <99160747+Muskandhamani1108@users.noreply.github.com>
2022-04-26 19:39:55 +0530

    Add files via upload

---
+++ array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of array:- ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] n = new int[size];
+            Console.WriteLine("Enter array elements:-");
+            for (int i = 0; i < size; i++)
+            {
+                n[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(".....Array.....");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(n[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
