bdef0cb8cfbc82a5cf93e0beda0dcfdc721abf2b
Muskandhamani1108 <99160747+Muskandhamani1108@users.noreply.github.com>
2022-04-26 22:53:05 +0530

    Add files via upload

---
+++ destructor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tablecons
+{
+    class cons
+    {
+        public cons(int n)
+        {
+            Console.WriteLine("Table of " + n + " is:-");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i * n);
+            }
+        }
+        ~cons()
+        {
+            Console.WriteLine("Destructor is called.....");
+            Console.ReadLine();
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the no:- ");
+            n = Convert.ToInt16(Console.ReadLine());
+            cons c1 = new cons(n);
+            Console.ReadLine();
+        }
+    }
+}
