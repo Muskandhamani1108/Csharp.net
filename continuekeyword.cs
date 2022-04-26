9d195a856e6fe103030a531ef37311114da11930
Muskandhamani1108 <99160747+Muskandhamani1108@users.noreply.github.com>
2022-04-26 19:41:46 +0530

    Add files via upload

---
+++ continuekeyword.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace loop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (i >= 6)
+                    {
+                        Console.WriteLine(i);
+                        continue;
+                    }
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
