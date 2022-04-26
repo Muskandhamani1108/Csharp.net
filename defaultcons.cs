db3041a25b170eefb6123790d45808fffc81796f
Muskandhamani1108 <99160747+Muskandhamani1108@users.noreply.github.com>
2022-04-26 22:52:46 +0530

    Add files via upload

---
+++ defaultcons.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dconstructor
+{
+    class cons
+    {
+        int age;
+        public cons()
+        {
+            age = 18;
+            Console.WriteLine(age);
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            cons a = new cons();
+            Console.ReadLine();
+        }
+    }
+}
