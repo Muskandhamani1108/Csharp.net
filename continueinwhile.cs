45ae817c497ce8df9dea3ffade88c69f17ee6a20
Muskandhamani1108 <99160747+Muskandhamani1108@users.noreply.github.com>
2022-04-26 19:41:15 +0530

    Add files via upload

---
+++ continueinwhile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace while2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 10)
+            {
+                i++;
+                if (i % 2 == 0)
+                {
+                    if (i >= 6)
+                    {
+                        Console.WriteLine(i);
+                        continue;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
