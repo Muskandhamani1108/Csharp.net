df874d14173cdc5e142ddddcf925be7b3a71e31c
Muskandhamani1108 <99160747+Muskandhamani1108@users.noreply.github.com>
2022-04-26 19:38:41 +0530

    Add files via upload

---
+++ add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace basicproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 2;
+            b = 2;
+            c = a + b;
+
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
