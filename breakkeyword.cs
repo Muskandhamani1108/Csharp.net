9f7579327361d8a4d9a0e920a6cbec6ceb1d116e
Muskandhamani1108 <99160747+Muskandhamani1108@users.noreply.github.com>
2022-04-26 19:40:47 +0530

    Add files via upload

---
+++ breakkeyword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace loop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                    if (i == 6)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ classobj.cs
@@ -0,0 +1,37 @@
+﻿/*
+    Que:-Demonstrate use of class and object.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classobj
+{
+    public class demo
+    {
+        public int r;
+        public string n;
+        public void read()
+        {
+            Console.WriteLine("Enter roll no:-");
+            r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Name:-");
+            n = Console.ReadLine();
+        }
+        public void show()
+        {
+            Console.WriteLine("Your Rollno is:- " + r);
+            Console.WriteLine("Your Name is:-" + n);
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            demo d1 = new demo();
+            d1.read();
+            d1.show();
+            Console.ReadLine();
+        }
+    }
+}
